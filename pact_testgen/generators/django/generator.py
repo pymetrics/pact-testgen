aeb9e6272f94024475e5b1b4f701fa0db0c36a13
djetter-py <david.jetter@pymetrics.com>
2021-08-12 16:42:00 -0400

    add really simple django testcase generator

---
+++ pact_testgen/generators/django/generator.py
@@ -0,0 +1,17 @@
+import os
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+path = os.path.dirname(__file__) + "/templates"
+print(path)
+env = Environment(
+    loader=FileSystemLoader(searchpath=path),
+    autoescape=select_autoescape()
+)
+
+method_names = ["method_1", "method_2"]
+case_name = "chris"
+methods = env.get_template("test_methods.jinja").render(method_names=method_names)
+case = env.get_template("test_case.jinja").render(case_name=case_name, test_methods=methods)
+
+print(case)
