286ea6438c435a2afeb42cfd0e99cf3df5e8ebc6
Lance Goodridge <ldgoodridge95@gmail.com>
2021-08-12 18:37:31 -0400

    Wired up pact file loading to CLI

--- pact_testgen/cli.py
+++ pact_testgen/cli.py
@@ -1,26 +1,32 @@
 """Console script for pact_testgen."""
 import argparse
+import json
 import sys
 
+from generators.django.generator import generate_tests
+from models import Pact
 
-def load_pact_file(path: str) -> str:
-    # just a dumb placeholder that throws an IOError if
-    # the file doesn't exist
-    with open(path) as f:
-        return f.read()
+
+def load_pact_file(path: str) -> Pact:
+    """ Loads the file at the supplied path into a Pact model """
+    with open(path, "r") as f:
+        pact = json.load(f)
+        return Pact(**pact)
+
+
+def generate_template(base_class: str, pact: Pact) -> str:
+    """ Converts the base class and Pact model into a template """
+    # TODO: replace this placeholder code
+    base_class_import_path, base_class = base_class.rsplit(".", 1)
+    template = generate_tests()
+    return template
 
 
 def run(base_class: str, pact_file: str):
-    # placeholder code, obviously
-    base_class_import_path, base_class = base_class.rsplit(".", 1)
-    load_pact_file(pact_file)
-    print(f"from {base_class_import_path} import {base_class}")
-    print("from pact_testgen.verify import verify_response")
-    print()
-    print(f"class SomeProviderStateTests({base_class}):")
-    print("    def test_some_integration(self):")
-    print("        resp = self.client.get('/resource/1')")
-    print("        verify_response(resp, matcher_goes_here_probably)")
+    """ Loads the pact file, and writes thei generated template(s) to stdout """
+    pact = load_pact_file(pact_file)
+    template = generate_template(base_class, pact)
+    print(template)
 
 
 def main():
@@ -30,7 +36,10 @@ def main():
     parser.add_argument(
         "--base-class",
         default="django.test.TestCase",
-        help="Python path to the TestCase which generated test cases will subclass.",
+        help=(
+            "Python path to the TestCase which generated test cases "
+            "will subclass."
+        ),
     )
     # Reserve -b for Pact Broker support
     args = parser.parse_args()
