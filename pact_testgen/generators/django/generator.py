fdb719c987c1ca1729a46df97152e8b9318b5ed5
djetter-py <david.jetter@pymetrics.com>
2021-08-12 17:57:57 -0400

    add function to generator

--- pact_testgen/generators/django/generator.py
+++ pact_testgen/generators/django/generator.py
@@ -9,9 +9,11 @@ env = Environment(
     autoescape=select_autoescape()
 )
 
-method_names = ["method_1", "method_2"]
-case_name = "TestChris"
-methods = env.get_template("test_methods.jinja").render(method_names=method_names)
-case = env.get_template("test_case.jinja").render(case_name=case_name, methods=methods)
 
-print(case)
+def generate_tests() -> str:
+    method_names = ["method_1", "method_2"]
+    case_name = "TestChris"
+    methods = env.get_template("test_methods.jinja").render(method_names=method_names)
+    case = env.get_template("test_case.jinja").render(case_name=case_name, methods=methods)
+
+    return case
