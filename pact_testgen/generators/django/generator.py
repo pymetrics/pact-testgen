f860bca3a8bd9046ee78a79b8e660437f01c8054
djetter-py <david.jetter@pymetrics.com>
2021-08-12 17:22:33 -0400

    add poetry

--- pact_testgen/generators/django/generator.py
+++ pact_testgen/generators/django/generator.py
@@ -10,7 +10,7 @@ env = Environment(
 )
 
 method_names = ["method_1", "method_2"]
-case_name = "chris"
+case_name = "TestChris"
 methods = env.get_template("test_methods.jinja").render(method_names=method_names)
 case = env.get_template("test_case.jinja").render(case_name=case_name, methods=methods)
 
