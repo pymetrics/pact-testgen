ffaf6681ed0ca2b21bdd251c850be8f4ef6ca018
Lance Goodridge <ldgoodridge95@gmail.com>
2021-08-13 13:25:16 -0400

    Got CLI working again

--- pact_testgen/generators/django/generator.py
+++ pact_testgen/generators/django/generator.py
@@ -1,3 +1,4 @@
+import json
 import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -19,8 +20,12 @@ def generate_tests(test_file: TestFile) -> str:
     for case in test_file.test_cases:
         for method in case.test_methods:
             args.append(_build_request_args(method))
-    methods = env.get_template("test_methods.jinja").render(method_names=method_names, expected_body=expected_body)
-    case = env.get_template("test_case.jinja").render(case_name=case_name, methods=methods)
+    methods = env.get_template("test_methods.jinja").render(
+        method_names=method_names, expected_body=expected_body
+    )
+    case = env.get_template("test_case.jinja").render(
+        case_name=case_name, file=test_file, methods=methods
+    )
 
     return case
 
@@ -29,13 +34,15 @@ def _build_request_args(interaction: Interaction) -> TestMethodArgs:
     request_args = RequestArgs(
         method=interaction.request.method.value,
         path=interaction.request.path,
-        data=interaction.request.body.json() if interaction.request.body else "",
+        data=json.dumps(interaction.request.body)
+        if interaction.request.body
+        else "",
     )
 
     test_method_args = TestMethodArgs(
         name=interaction.description,
         expectation=repr(interaction.response.dict()),
-        request=request_args
+        request=request_args,
     )
 
     return test_method_args
