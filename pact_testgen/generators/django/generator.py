af4887e6377109f9a3a12dbd59f0557db5ec08a3
djetter-py <david.jetter@pymetrics.com>
2021-08-13 12:23:46 -0400

    use models for templates

--- pact_testgen/generators/django/generator.py
+++ pact_testgen/generators/django/generator.py
@@ -1,12 +1,10 @@
 import os
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-from pact_testgen.models import TestFile
-
+from pact_testgen.generators.models import RequestArgs, TestMethodArgs
+from pact_testgen.models import TestFile, Interaction
 
 path = os.path.dirname(__file__) + "/templates"
-print(path)
 env = Environment(
     loader=FileSystemLoader(searchpath=path), autoescape=select_autoescape()
 )
@@ -16,9 +14,28 @@ def generate_tests(test_file: TestFile) -> str:
     # TODO: Actually use the test_case
     method_names = ["method_1", "method_2"]
     case_name = "TestChris"
-    methods = env.get_template("test_methods.jinja").render(method_names=method_names)
-    case = env.get_template("test_case.jinja").render(
-        case_name=case_name, methods=methods
-    )
+    expected_body = {"key1": "val1", "key2": {"nested_key1": "val2"}}
+    args = []
+    for case in test_file.test_cases:
+        for method in case.test_methods:
+            args.append(_build_request_args(method))
+    methods = env.get_template("test_methods.jinja").render(method_names=method_names, expected_body=expected_body)
+    case = env.get_template("test_case.jinja").render(case_name=case_name, methods=methods)
 
     return case
+
+
+def _build_request_args(interaction: Interaction) -> TestMethodArgs:
+    request_args = RequestArgs(
+        method=interaction.request.method.value,
+        path=interaction.request.path,
+        data=interaction.request.body.json() if interaction.request.body else None,
+    )
+
+    test_method_args = TestMethodArgs(
+        name=interaction.description,
+        expectation=repr(interaction.response.dict()),
+        request=request_args
+    )
+
+    return test_method_args
