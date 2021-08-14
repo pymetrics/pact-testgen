69bd6305a64f72ba891eddf6b7463cf86b5fc4d4
Chris Lawlor <chris@pymetrics.com>
2021-08-14 13:11:25 -0400

    Add json method to utils.Response

--- pact_testgen/utils.py
+++ pact_testgen/utils.py
@@ -1,9 +1,13 @@
+import json
 from dataclasses import dataclass, field
 from typing import Any, Dict
 
 
 @dataclass
 class Response:
+    """
+    Requests-like Response class.
+    """
     text: str
     headers: Dict[str, Any] = field(default_factory=dict)
     status_code: int = 200
@@ -15,6 +19,9 @@ class Response:
             headers=dict(response.headers),
             status_code=response.status_code,
         )
+
+    def json(self):
+        return json.loads(self.text)
 
 
 def to_camel_case(value: str) -> str:
