1d835749d0fcec20456e8155f6722d10aba090fc
Chris Lawlor <chris@pymetrics.com>
2021-08-18 16:42:55 -0400

    Check for expected tests in generator output string

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,9 +3,9 @@ from pact_testgen.generators.django.generator import generate_tests
 
 def test_django_test_generator_output_is_parsable(testfile):
     output = generate_tests(testfile)
-    code_obj = compile(output, "<string>", "exec")
+    compile(output, "<string>", "exec")
     # Names of test cases we expect to see. This is driven directly
     # by test_app/client_tests.py
-    assert "TestAnAuthorWithId1Exists" in code_obj.co_names
-    assert "TestABookExistsWithAuthorId1AnAuthorWithId1Exists" in code_obj.co_names
-    assert "TestNothing" in code_obj.co_names
+    assert "TestAnAuthorWithId1Exists" in output
+    assert "TestABookExistsWithAuthorId1AnAuthorWithId1Exists" in output
+    assert "TestNothing" in output
